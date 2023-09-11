37955fc9df2b43601aace4d8db8af1d0929f28fc
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-09-10 19:25:31 -0500

    Added calc_addr_with_bytes

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,7 @@ use rand::{distributions::Alphanumeric, Rng};
 use sha3::{Digest, Keccak256};
 
 // TODO: create fn that accepts salt as &[u8; 32] instead of just &[u8]
-// TODO: rename functions in major release to make sense with previously mentioned fn
-// TODO: should panic/return a Result object if prefix/suffix has incorrect characters
+// TODO: rename functions in major release to make sense with previously mentioned fn// TODO: should panic/return a Result object if prefix/suffix has incorrect characters
 // TODO: add regex fn for salt generation
 // TODO: convert panics into Results
 // TODO: rename the salt_suffix argument to salt_prefix
@@ -29,10 +28,25 @@ pub fn calc_addr(deployer: &[u8], salt: &[u8]) -> [u8; 20] {
     // [contract creation prefix] + [create3 deployer] + [salt] + [keccak256(childBytecode)]
     let salt_hash = Keccak256::digest(salt);
     // println!("Salt hash: 0x{}", hex::encode(&salt_hash));
+
+    calc_addr_with_bytes(deployer, &salt_hash.as_slice()[0.. 32].try_into().unwrap())
+}
+
+/// Calculates the address of a contract based on the given deployer and salt.
+/// 
+/// # Arguments
+///
+/// * `deployer` - A byte slice representing the create3 deployer address.
+/// * `salt` - Bytes in u8 array format that is directly used as the salt input.
+///
+/// # Returns
+///
+/// A 20-byte array representing the address of the contract.
+pub fn calc_addr_with_bytes(deployer: &[u8], salt: &[u8; 32]) -> [u8; 20] {
     let mut bytes: Vec<u8> = Vec::new();
     bytes.push(0xff);
     bytes.extend_from_slice(deployer);
-    bytes.extend_from_slice(&salt_hash);
+    bytes.extend_from_slice(salt);
     bytes.extend_from_slice(&KECCAK256_PROXY_CHILD_BYTECODE);
     let hash = Keccak256::digest(&bytes);
     let mut proxy_bytes = [0u8; 20];
@@ -140,10 +154,10 @@ pub fn generate_salt_suffix(deployer: &[u8], salt_suffix: &str, prefix: &str) ->
 #[cfg(test)]
 mod tests {
     use sha3::{Digest, Keccak256};
-    use crate::{calc_addr, generate_salt, generate_salt_suffix, KECCAK256_PROXY_CHILD_BYTECODE};
+    use crate::{calc_addr, generate_salt, generate_salt_suffix, KECCAK256_PROXY_CHILD_BYTECODE, calc_addr_with_bytes};
 
     #[test]
-    fn should_calculate_correctly_with_given_salt() {
+    fn should_calculate_correctly_with_given_salt_string() {
         let deployer: &Vec<u8> = &hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap();
 
         // Answers were generated with the Solady CREATE3 library
@@ -164,6 +178,27 @@ mod tests {
     }
 
     #[test]
+    fn should_calculate_correctly_with_given_salt() {
+        let deployer = &hex::decode("d8b934580fcE35a11B58C6D73aDeE468a2833fa8").unwrap();
+
+        // Answers were generated with the Solady CREATE3 library
+        // https://github.com/Vectorized/solady/blob/main/src/utils/CREATE3.sol
+        let correct_answers: Vec<(&str, &str)> = vec![
+            ("3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb", "442188F25da4ac213D55aE81F1BFB421a4eb4562"),
+            ("b5553de315e0edf504d9150af82dafa5c4667fa618ed0a6f19c69b41166c5510", "551b9d8A7106Fdf98e68c4bf12Da1f23ad70C815"),
+            ("0b42b6393c1f53060fe3ddbfcd7aadcca894465a5a438f69c87d790b2299b9b2", "43d8e8C69fd771f7D3F4e25697Dadd3cC11D1cDB"),
+            ("ead17456afde832907c72ba39033455130a8f4d540a869ba31312c2746bf9c4b", "AB3D55404C5C21D18403A71aF5f6887BD0EC8d56")
+        ];
+
+        for (salt, answer) in correct_answers.iter() {
+            let salt: [u8; 32] = hex::decode(*salt).unwrap()[0.. 32].try_into().unwrap();
+            let addr: [u8; 20] = calc_addr_with_bytes(deployer, &salt);
+            let addr_str = hex::encode(addr);
+            assert_eq!(addr_str, answer.to_lowercase());
+        }
+    }
+
+    #[test]
     fn should_generate_with_prefix() {
         let deployer: &Vec<u8> = &hex::decode("5e17b14ADd6c386305A32928F985b29bbA34Eff5").unwrap();
         let runs = vec!["0", "00", "000", "abc", "123", "789"];
@@ -171,30 +206,13 @@ mod tests {
         for run in runs.iter() {
             let salt = generate_salt(deployer, run);
             
-            // NOTE:    would rather the library expose a function. Waiting until major release to do so
-            //          let addr: [u8; 20] = calc_addr_with_bytes(deployer, &salt);
-            let mut bytes: Vec<u8> = Vec::new();
-            bytes.push(0xff);
-            bytes.extend_from_slice(deployer);
-            bytes.extend_from_slice(&salt);
-            bytes.extend_from_slice(&KECCAK256_PROXY_CHILD_BYTECODE);
-            let hash = Keccak256::digest(&bytes);
-            let mut proxy_bytes = [0u8; 20];
-            proxy_bytes.copy_from_slice(&hash[12..]);
-        
-            // Use proxy address to compute the final contract address.
-            // keccak256(rlp(proxy_bytes ++ 0x01)) More here -> https://ethereum.stackexchange.com/a/761/66849
-            let mut bytes2: Vec<u8> = Vec::new();
-            bytes2.extend_from_slice(&[0xd6, 0x94]); // RLP prefix for a list of two items
-            bytes2.extend(&proxy_bytes); // The proxy address
-            bytes2.push(0x01); // The nonce of the contract
-            let hash2 = Keccak256::digest(&bytes2);
-        
-            // resulting hash -> The last 20 bytes (40 characters) of the hash.
-            let mut address = [0u8; 20];
-            address.copy_from_slice(&hash2[12..]);
+            /* NOTE: 
+             * This essentially repeats the code in generate_salt. Could be useful for future changes of the function. 
+             * Is there a better way of testing this?
+            */ 
+            let addr: [u8; 20] = calc_addr_with_bytes(deployer, &salt);
 
-            assert!(hex::encode(address).starts_with(run));
+            assert!(hex::encode(addr).starts_with(run));
         }
     }
 
